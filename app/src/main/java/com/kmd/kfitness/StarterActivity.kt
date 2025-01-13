f773dcc9603202187cbfdc73b2ace11467efb529
Chit Swe <chitswe.dev@gmail.com>
2025-01-14 00:37:53 +0630

    fix Stater Activity show only white screen

--- app/src/main/java/com/kmd/kfitness/StarterActivity.kt
+++ app/src/main/java/com/kmd/kfitness/StarterActivity.kt
@@ -1,12 +1,9 @@
 package com.kmd.kfitness
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -20,10 +17,11 @@ class StarterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStarterBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_starter)
+        setContentView(binding.root)
        val navHost =  supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_starter) as NavHostFragment;
         navController = navHost.navController;
     }
+
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
